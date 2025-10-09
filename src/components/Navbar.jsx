e0de9517ca5ebe6c1a2ab6de764bf7e15cd3346b
IHuy354 <leduchuy.vng2004@gmail.com>
2025-10-09 21:18:32 +0700

    fix ui nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,97 @@
 import { NavLink } from "react-router-dom";
 import "../styles/Navbar.css";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import React from "react";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 export default function Navbar() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <nav
       style={{
-        display: "flex",
-        gap: "20px",
-        justifyContent: "center",
-        alignItems: "center",
         background: "rgb(18, 195, 139)",
         height: "50px",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "20px",
+        position: "relative",
       }}
-      className="nav-main"
     >
-      <NavLink to="/" end>Bảng điều khiển</NavLink>
-      <NavLink to="/transactions">Giao dịch</NavLink>
-      <NavLink to="/stats">Thống kê</NavLink>
-      <NavLink to="/settings">Cài đặt</NavLink>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "10px",
+        }}
+      >
+        <Avatar alt="lỗi logo" src="../../public/logo.jpg" />
+        <p style={{ fontWeight: "530", color: "white", fontSize: "21px" }}>
+          Finaking
+        </p>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          gap: "40px",
+          justifyContent: "center",
+          alignItems: "center",
+          position: "absolute",
+          left: "50%",
+          transform: "translateX(-50%)",
+        }}
+        className="nav-main"
+      >
+        <NavLink style={{ fontSize: "19px" }} to="/" end>
+          Bảng điều khiển
+        </NavLink>
+        <NavLink style={{ fontSize: "19px" }} to="/transactions">
+          Giao dịch
+        </NavLink>
+        <NavLink style={{ fontSize: "19px" }} to="/stats">
+          Thống kê
+        </NavLink>
+      </div>
+
+      <Button
+        onClick={handleClick}
+        style={{ display: "flex", gap: "7px" }}
+        sx={{ color: "white" }}
+      >
+        <Avatar alt="lỗi logo" src="../../public/logo.jpg" />
+        <div style={{ display: "flex" }}>
+          <p>Huy Lê</p>
+          <ExpandMoreIcon />
+        </div>
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        slotProps={{
+          list: {
+            "aria-labelledby": "basic-button",
+          },
+        }}
+      >
+        <MenuItem onClick={handleClose}>
+          <NavLink to="/settings">Cài đặt</NavLink>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>Tài khoản</MenuItem>
+        <MenuItem onClick={handleClose}>Đăng xuất</MenuItem>
+      </Menu>
     </nav>
   );
 }
